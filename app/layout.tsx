c3e5f54b418fd0fdc38fdc29e7694ac7eb956201
Yousuftayyaba <Yousuftayyaba2@gmail.com>
2024-12-13 15:32:05 +0500

    changes for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import "./globals.css";
-import { Roboto, Poppins, Inter } from "next/font/google";
+import { Poppins, Inter } from "next/font/google";
 
-const myfont = Roboto ({
-  subsets : ['latin'],
-  weight: ["400", "700"],
-})
 const myfontfamily = Poppins ({
   subsets : ['latin'],
   weight: ["100","300", "700"],
@@ -21,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${myfont.className} ${myfontfamily.className} ${mythirdfont.className}`}>
+        className={`${myfontfamily.className} ${mythirdfont.className}`}>
       
         {children}
       </body>
